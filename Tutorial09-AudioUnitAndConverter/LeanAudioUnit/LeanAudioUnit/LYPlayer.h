3542d964139a53eba32be364f29b6275c9a13d1a
loyinglin <loying@foxmail.com>
2017-09-26 17:02:52 +0800

    修正。

---
+++ Tutorial09-AudioUnitAndConverter/LeanAudioUnit/LeanAudioUnit/LYPlayer.h
@@ -0,0 +1,26 @@
+
+//  LeanAudioUnit
+//
+//  Created by loyinglin on 2017/9/26.
+//  Copyright © 2017年 loyinglin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class LYPlayer;
+@protocol LYPlayerDelegate <NSObject>
+
+- (void)onPlayToEnd:(LYPlayer *)player;
+
+@end
+
+
+@interface LYPlayer : NSObject
+
+@property (nonatomic, weak) id<LYPlayerDelegate> delegate;
+
+- (void)play;
+
+- (double)getCurrentTime;
+
+@end
---
+++ Tutorial09-AudioUnitAndConverter/LeanAudioUnit/LeanAudioUnit/ViewController.h
@@ -0,0 +1,13 @@
+
+//  LeanAudioUnit
+//
+//  Created by loyinglin on 2017/9/26.
+//  Copyright © 2017年 loyinglin. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
